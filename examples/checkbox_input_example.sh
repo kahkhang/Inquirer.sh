da95e5ac758b408c71ea45fd9ca72e72bc2623a2
Andrew Low <andrew.low@ninjavan.co>
2017-06-27 18:34:26 +0800

    make source directories relative

--- examples/checkbox_input_example.sh
+++ examples/checkbox_input_example.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-source checkbox_input.sh
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+PARENT_DIR=$(dirname "$DIR")
+source $PARENT_DIR/checkbox_input.sh
 
 hawker_centres=( 'Old Airport Road Hawker Centre' 'Golden Mile Food Complex' 'Maxwell Food Centre' 'Newton Food Centre' )
 checkbox_input "Which hawker centres do you prefer?" hawker_centres selected_hawkers
--- examples/list_input_example.sh
+++ examples/list_input_example.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-source list_input.sh
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+PARENT_DIR=$(dirname "$DIR")
+source $PARENT_DIR/list_input.sh
 
 drinks=( 'Teh' 'Teh Ping Gao Siu Dai' 'Kopi O' 'Yuan Yang' )
 list_input "What would you like to drink today (1st Drink)?" drinks selected_drink
